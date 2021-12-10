408d775a4685f47341aab05b042061ebeeb1a490
Jake Dustin <jakedustin097@gmail.com>
2021-12-10 14:29:22 -0700

    removed comment from main

--- primary/src/main/java/com/qualtrics/jdustin/App.java
+++ primary/src/main/java/com/qualtrics/jdustin/App.java
@@ -5,10 +5,6 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
 public class App {
     public static void main(String[] args) {
         try {
